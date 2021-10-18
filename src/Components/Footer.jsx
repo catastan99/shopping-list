a4fc121b17f6f7a37d161bbafe94b0f4567a508c
catastan99 <catalin.stan7@yahoo.com>
2021-10-18 09:15:38 +0300

    Footer updates

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import '../style/Footer.css';
+import {ReactComponent as EmailIcon} from '../assets/icons/EmailIcon.svg';
+import {ReactComponent as GithubIcon} from '../assets/icons/GithubIcon.svg';
+import {ReactComponent as LinkedinIcon} from '../assets/icons/LinkedinIcon.svg'
 
 function Footer() {
     return (
         <div className="Footer">
             <p>&copy; Catalin Stan</p>
-            <p>2021</p>
+            <p><LinkedinIcon /> <a href="https://www.linkedin.com/in/catalinstan99/">LinkedIn</a></p>           
+            <p><GithubIcon /> <a href="https://github.com/catastan99"> GitHub</a></p>
+            <p><EmailIcon /> <a href="mailto:catalin.stan7@yahoo.com">catalin.stan7@yahoo.com</a></p>
         </div>
     )
 }
